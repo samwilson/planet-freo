56a3c899b16158b8a0682c36179d03ee8fc64d9c
Sam Wilson <sam@samwilson.id.au>
2015-12-10 05:54:10 +0800

    Freo settings.

--- custom/views/default/top.tpl.php
+++ custom/views/default/top.tpl.php
@@ -1,3 +1,15 @@
         <div id="header">
-            <h1 id="top"><a href="<?php echo $PlanetConfig->getUrl() ?>"><?php echo $PlanetConfig->getName(); ?></a></h1>
+            <h1 id="top"><a href="./index.php"><?php echo $PlanetConfig->getName(); ?></a></h1>
+<p>This is <strong>Planet Freo</strong>, an aggregation of numerous blogs' and 
+other websites' news feeds, all relating to <strong>Fremantle</strong> in some way.
+Brought to you by the <a href="http://fremantlesociety.org.au">Fremantle Society</a>.
+If you would like to have your site included here (anything about
+Fremantle is appropriate), or see a list of websites whose feeds <em>would</em>
+probably be included here <em>if they had them</em>, please see
+<a href="http://freo.org.au/wiki/Planet_Freo">freo.org.au/wiki/Planet_Freo</a>.
+</p>
+<p>For a succinct list of the most recent headlines (as below, but without excerpts), see <a href="index.php?type=archive">the archive</a>.
+<p>
+Enquires may be addressed to <a href="mailto:sam@samwilson.id.au">sam@samwilson.id.au</a>.
+</p>
         </div>
